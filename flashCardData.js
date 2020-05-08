1b0d84d701536b3861c4ebe00afdeaabf607ab7a
gnvvbll <gnvvbll@gmail.com>
2020-05-08 16:28:16 -0400

    Initial Commit

---
+++ flashCardData.js
@@ -0,0 +1,77 @@
+var importedFlashCardDataFile = [{
+    front: {
+      question: 'In the 90\'s, Britney Spears dated the member of NSYNC _________.',
+      hint: 'He\'s the only one with a career after the boyband disbanded.'
+    },
+    back: {
+      image: './images/jt.jpg',
+      answer: 'Justin Timberlake',
+      description: 'Even though they broke our hearts when they broke up, they at least gave us this ICONIC couples outfit.'
+    }
+  },
+  {
+    front: {
+      question: '28,000 people went to Spike Island on the Mersey estuary in May 1990 to see the band _________.',
+      hint: 'Think Rocks and Flowers.'
+    },
+    back: {
+      image: './images/stoneroses.jpg',
+      answer: 'The Stone Roses',
+      description: 'Honestly, if I had been around in 1990, I would have gone too. If you aren\'t familiar with Stone Roses, do yourself a favor and listen to "I Wanna be Adored."'
+    }
+  },
+  {
+    front: {
+      question: 'The name of Christina Aguilera\'s debut single was _________.',
+      hint: 'She is requesting that someone lets her out of something.'
+    },
+    back: {
+      image: './images/xtina.jpg',
+      answer: 'Genie in a Bottle',
+      description: 'When this came out I danced around to it in my room at least a million times, I was six years old. Had no idea what she was talking about.'
+    }
+  },
+  {
+    front: {
+      question: 'Jay-Z\'s "Hard Knock Life (Ghetto Anthem)" samples a song from the musical _________.',
+      hint: 'Sarah Jessica Parker starred in this as the titular character on broadway when she was a kid.'
+    },
+    back: {
+      image: './images/annie.jpg',
+      answer: 'Annie',
+      description: 'Does anyone remember how much of an alcoholic mess Mrs. Hannigan was? And that she was completely abusive? Is it weird that I kind of love her?'
+    }
+  },
+  {
+    front: {
+      question: '_________ released the multi-platinum 1995 album "Jagged Little Pill."',
+      hint: 'She is pretty much one of the most influential female singer-songwriters of the 90\'s.'
+    },
+    back: {
+      image: './images/alanis.jpg',
+      answer: 'Alanis Morissette',
+      description: 'It\'s insane that the majority of the songs on this album are about her breakup with David Coulier, who played Uncle Joey on Full House. Like, really? HE is who broke your heart? "CUT IT OUUUT."'
+    }
+  },
+  {
+    front: {
+      question: '_________ featured the Queen and David Bowie song "Under Pressure" in their 1990 hit.',
+      hint: 'This person is absurd.'
+    },
+    back: {
+      image: './images/vanillaice.jpg',
+      answer: 'Vanilla Ice',
+      description: 'When he was called out for not crediting Queen and Bowie, he claimed his song was different. He said,"No no, that song goes da-da-da-DA-da-da, mine goes da-da-da-da-DA-da."'
+    }
+  },
+  {
+    front: {
+      question: 'Cordozar Calvin Broadus Junior is more commonly known as _________.',
+      hint: 'He is a rapper.'
+    },
+    back: {
+      image: './images/snoop.jpg',
+      answer: 'Snoop Dogg',
+      description: 'While Snoop Dogg is an awesome name (and it is a name that pays homage to his starting group Tha Dogg Pound), Cordozar Calvin Broadus Junior is a pretty epic name.'
+    }
+  }];
